0b2fdc879346d782eb082e9883aee0398344e20c
Matthew Nelson <05nelsonm@protonmail.com>
2019-10-21 15:15:20 -0400

    built out initial data models

--- app/src/main/java/com/lambdaschool/devlibs/model/ApiDataModels.kt
+++ app/src/main/java/com/lambdaschool/devlibs/model/ApiDataModels.kt
@@ -1,2 +1,22 @@
 package com.lambdaschool.devlibs.model
 
+class RegistrationSendAPI(
+    val username: String,
+    val password: String
+)
+
+data class RegistrationReturnedAPI(
+    val message: String,
+    val user_id: Int
+)
+
+class LoginSendAPI(
+    val username: String,
+    val password: String
+)
+
+data class LoginReturnedAPI(
+    val message: String,
+    val token: String,
+    val user_id: Int
+)
