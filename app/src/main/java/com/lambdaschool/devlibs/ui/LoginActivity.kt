882e3c326f15b9b563de25afe57502e3c92f65a4
Matthew Nelson <05nelsonm@protonmail.com>
2019-10-21 13:54:11 -0400

    built out basic app structure and dependencies

--- app/src/main/java/com/lambdaschool/devlibs/ui/LoginActivity.kt
+++ app/src/main/java/com/lambdaschool/devlibs/ui/LoginActivity.kt
@@ -1,12 +1,13 @@
-package com.lambdaschool.devlibs
+package com.lambdaschool.devlibs.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lambdaschool.devlibs.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
