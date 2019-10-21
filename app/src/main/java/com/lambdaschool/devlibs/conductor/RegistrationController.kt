18102a4480bf364c1f44bb232b21c97153816db2
jack seymour <stilljack@gmail.com>
2019-10-21 10:53:16 -0700

    init

---
+++ app/src/main/java/com/lambdaschool/devlibs/conductor/RegistrationController.kt
@@ -0,0 +1,15 @@
+package com.lambdaschool.devlibs.conductor
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import work.beltran.conductorviewmodel.ViewModelController
+
+class RegistrationController (bundle: Bundle) : ViewModelController(bundle)  {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
