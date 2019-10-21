9b6c9f89f2876634cd1ffaf19fe6a75f5520f813
Matthew Nelson <05nelsonm@protonmail.com>
2019-10-21 17:46:56 -0400

    reconfigured shared prefs for login credentials

--- app/src/main/java/com/lambdaschool/devlibs/Prefs.kt
+++ app/src/main/java/com/lambdaschool/devlibs/Prefs.kt
@@ -2,14 +2,15 @@ package com.lambdaschool.devlibs
 
 import android.content.Context
 import android.content.SharedPreferences
-import com.lambdaschool.devlibs.model.LoginReturnedAPI
+import com.lambdaschool.devlibs.model.LoginSuccess
 
 class Prefs(context: Context) {
 
     companion object {
         private const val LOGIN_PREFERENCES = "LoginPrefs"
-        private const val LOGIN_TOKEN_KEY = "LoginToken"
         private const val LOGIN_USER_ID_KEY = "LoginUserId"
+        private const val LOGIN_USER_NAME_KEY = "LoginUserName"
+        private const val LOGIN_TOKEN_KEY = "LoginToken"
         private const val INVALID_STRING = "INVALID"
         private const val INVALID_INT = -1
     }
@@ -17,32 +18,47 @@ class Prefs(context: Context) {
     private val sharedPrefs: SharedPreferences =
         context.getSharedPreferences(LOGIN_PREFERENCES, Context.MODE_PRIVATE)
 
-    fun createLoginCredentialEntry(entry: LoginReturnedAPI) {
+    fun createLoginCredentialEntry(entry: LoginSuccess) {
         val editor = sharedPrefs.edit()
+        editor.putInt(LOGIN_USER_ID_KEY, entry.userId)
+        editor.putString(LOGIN_USER_NAME_KEY, entry.username)
         editor.putString(LOGIN_TOKEN_KEY, entry.token)
-        editor.putInt(LOGIN_USER_ID_KEY, entry.user_id)
         editor.apply()
     }
 
-    fun getLoginCredentials(): LoginReturnedAPI? {
+    /**
+     * This method is for obtaining shared preferences for user login credentials
+     * such that a simple null check can be made and then roll right into logging in.
+     * */
+    fun getLoginCredentials(): LoginSuccess? {
+
+        val userId = sharedPrefs.getInt(LOGIN_USER_ID_KEY, INVALID_INT)
+
+        var username = INVALID_STRING
+        sharedPrefs.getString(LOGIN_USER_NAME_KEY, INVALID_STRING)?.let {
+            username = it
+        }
+
         var token = INVALID_STRING
         sharedPrefs.getString(LOGIN_TOKEN_KEY, INVALID_STRING)?.let {
             token = it
         }
 
-        val userId = sharedPrefs.getInt(LOGIN_USER_ID_KEY, INVALID_INT)
-
-        return if (token != INVALID_STRING && userId != INVALID_INT) {
-            LoginReturnedAPI("", token, userId)
+        return if (userId != INVALID_INT && username != INVALID_STRING && token != INVALID_STRING) {
+            LoginSuccess(userId, username, token)
         } else {
             null
         }
     }
 
+    /**
+     * Simply overwrites credentials with invalid data
+     * */
     fun deleteLoginCredentials() {
         val editor = sharedPrefs.edit()
-        editor.putString(LOGIN_TOKEN_KEY, "")
         editor.putInt(LOGIN_USER_ID_KEY, INVALID_INT)
+        editor.putString(LOGIN_USER_NAME_KEY, INVALID_STRING)
+        editor.putString(LOGIN_TOKEN_KEY, INVALID_STRING)
         editor.apply()
     }
 
