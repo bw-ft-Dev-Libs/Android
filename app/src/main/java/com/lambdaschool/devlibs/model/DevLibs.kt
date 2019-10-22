61a44dab977c6b23b8112e64ebf739248d97f6fc
Matthew Nelson <05nelsonm@protonmail.com>
2019-10-21 22:25:43 -0400

    built out the devlib data models

--- app/src/main/java/com/lambdaschool/devlibs/model/DevLibs.kt
+++ app/src/main/java/com/lambdaschool/devlibs/model/DevLibs.kt
@@ -1,2 +1,41 @@
 package com.lambdaschool.devlibs.model
 
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+class DevLibCreate(
+    val lib: String,
+    @SerializedName ("user_id") val userId: Int,
+    @SerializedName ("category_id") val categoryId: Int
+)
+
+@Entity(tableName = "dev_lib_backend")
+data class DevLibBackend(
+
+    @PrimaryKey(autoGenerate = false) @NonNull
+    var id: Int,
+
+    var lib: String,
+    @SerializedName ("user_id") val userId: Int,
+    @SerializedName ("category_id") var categoryId: Int
+)
+
+/**
+ * Store the Dev Lib the user creates locally if connectivity drops.
+ *
+ * Will be able to populate as DevLibCreate, send to the backend,
+ * receive back the DevLibBackend class as a response, store it in
+ * DevLibBackend schema, then delete the DevLibLocal entry.
+ * */
+@Entity(tableName = "dev_lib_local")
+class DevLibLocal(
+
+    @PrimaryKey(autoGenerate = true) @NonNull
+    val id: Int = 0,
+
+    var lib: String,
+    val userId: Int,
+    var categoryId: Int
+)
