abba507f7471e58665c0cfac3e2f65c23e0ae507
Matthew Nelson <05nelsonm@protonmail.com>
2019-10-23 13:33:21 -0400

    extended toasts and opening the soft keyboard

--- app/src/main/java/com/lambdaschool/devlibs/Extensions.kt
+++ app/src/main/java/com/lambdaschool/devlibs/Extensions.kt
@@ -1,2 +1,16 @@
 package com.lambdaschool.devlibs
 
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+
+fun Context.showToast(string: String) {
+    Toast.makeText(this, string, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.openSoftKeyboard(view: View?) {
+    view?.requestFocus()
+    val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+}
